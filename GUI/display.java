2dca66048caac2823cedc89c0aac8cde16b24241
ThDat1016 <6351071016@st.utc2.edu.vn>
2024-04-07 00:35:19 +0700

    add images

--- GUI/display.java
+++ GUI/display.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package quanlycuahanggiay_java;
+package quanlycuahanggiay_java.GUI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
